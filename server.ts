ebf706a8c78045a859570b801e08c87d6aadbb53
Argus <argushakizimana@gmail.com>
2024-07-14 09:37:57 +1000

    Cors-enabled

--- server.ts
+++ server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import authRoutes from './routes/authRoutes';
 import menuRoutes from './routes/menuItem';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Use CORS middleware
+app.use(cors({
+  origin: 'http://localhost:3000', // Update this with your frontend's URL
+}));
+
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/menu', menuRoutes);
 
